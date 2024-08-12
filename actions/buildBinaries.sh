6828e4dd0bddc231ee1904f56f2787efe5b3ea26
Garry Sharp <>
2024-08-12 04:04:41 +0400

    🔒️ Added sha256 checksums to binaries

--- actions/buildBinaries.sh
+++ actions/buildBinaries.sh
@@ -19,25 +19,28 @@ mkdir -p "$build_dir"
 app_name="sharder"
 version=$(git describe --tags --always)
 
+rm -rf $build_dir/*
+
 $(
     cd $src_dir
-    #GOOS=linux GOARCH=amd64 go build -o $build_dir/$app_name-$version-linux-amd64 main.go
-
+    GOOS=linux GOARCH=amd64 go build -o $build_dir/$app_name-$version-linux-amd64 main.go
+    shasum $build_dir/$app_name-$version-linux-amd64 >> $build_dir/$app_name-$version-linux-amd64.sha256
     # Windows (x86_64)
-    #GOOS=windows GOARCH=amd64 go build -o $build_dir/$app_name-$version-windows-amd64.exe main.go
-
+    GOOS=windows GOARCH=amd64 go build -o $build_dir/$app_name-$version-windows-amd64.exe main.go
+    shasum $build_dir/$app_name-$version-windows-amd64 >> $build_dir/$app_name-$version-windows-amd64.sha256
     # macOS (x86_64)
     GOOS=darwin GOARCH=amd64 go build -o $build_dir/$app_name-$version-darwin-amd64 main.go
-
+    shasum $build_dir/$app_name-$version-darwin-amd64 >> $build_dir/$app_name-$version-darwin-amd64.sha256
     # Linux (ARM)
-    #GOOS=linux GOARCH=arm GOARM=7 go build -o $build_dir/$app_name-$version-linux-arm main.go
-
+    GOOS=linux GOARCH=arm GOARM=7 go build -o $build_dir/$app_name-$version-linux-arm main.go
+    shasum $build_dir/$app_name-$version-linux-arm >> $build_dir/$app_name-$version-linux-arm.sha256
     # Linux (ARM64)
-    #GOOS=linux GOARCH=arm64 go build -o $build_dir/$app_name-$version-linux-arm64 main.go
-
+    GOOS=linux GOARCH=arm64 go build -o $build_dir/$app_name-$version-linux-arm64 main.go
+    shasum $build_dir/$app_name-$version-linux-arm64 >> $build_dir/$app_name-$version-linux-arm64.sha256
     # Windows (ARM64)
-    #GOOS=windows GOARCH=arm64 go build -o $build_dir/$app_name-$version-windows-arm64.exe main.go
-
+    GOOS=windows GOARCH=arm64 go build -o $build_dir/$app_name-$version-windows-arm64.exe main.go
+    shasum $build_dir/$app_name-$version-windows-arm64 >> $build_dir/$app_name-$version-windows-arm64.sha256
     # macOS (ARM64)
-    #GOOS=darwin GOARCH=arm64 go build -o $build_dir/$app_name-$version-darwin-arm64 main.go
+    GOOS=darwin GOARCH=arm64 go build -o $build_dir/$app_name-$version-darwin-arm64 main.go
+    shasum $build_dir/$app_name-$version-darwin-arm64 >> $build_dir/$app_name-$version-darwin-arm64.sha256
 )
